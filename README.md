eef06913b9eaf60c8c8eba14bab2469e33ebefb8
jonathan <jonh.nobre@gmail.com>
2024-07-21 16:51:30 -0300

    finished the project and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Projeto-Sap
+
+Sistema de Armazenamento de Personagens (SAP) é uma aplicação WEB feita no Apache Netbeans IDE 21, utilizando o servidor Glassfish e o Java EE 7 Web.
+
+O arquivo SAP.sql possui o script para o banco de dados, já o arquivo mysql-connector-java-8.0.27.jar é a biblioteca para o conector do mysql.
+
+O sistema possui criptografia de senha, os usuários: admin@admin.admin, notadmin@user.admin e jojo@user.admin; todos possuem a mesma senha "admin", pelos seus cargos admin, moderador e usuario; as telas mudam de acordo com o cargo
--- SAP.sql
+++ SAP.sql
@@ -29,27 +29,6 @@ create table if not exists SAP.chara (
     foreign key (id_usuario_fk) references usuario(id_usuario)
 ) engine = InnoDB;
 
-/*
-alter table comentario
-add column editado_comentario boolean default false;
-
-create table if not exists SAP.atributo (
-	id int not null auto_increment,
-    nome varchar(20) not null,
-    primary key (id)
-) engine = InnoDB;
-
-
-create table if not exists SAP.chara_atributo (
-	id_atributo int not null,
-    id_chara int not null,
-    valor int not null,
-    primary key (id_atributo, id_chara),
-    foreign key (id_atributo) references atributo(id),
-    foreign key (id_chara) references chara(id)
-) engine = InnoDB;
-*/
-
 create table if not exists SAP.categoria(
 	id_categoria int not null auto_increment,
     id_usuario_fk int not null,
@@ -85,40 +64,19 @@ create table if not exists SAP.chat_mensagem(
     id_usuario_fk int not null,
     id_chat_fk int not null,
     mensagem_cm varchar(300) not null,
-	data_hora_cm datetime default(now()),
+	data_hora_cm datetime default now(),
     primary key (id_cm),
     foreign key (id_usuario_fk) references usuario(id_usuario),
     foreign key (id_chat_fk) references chat(id_chat)
 ) engine = InnoDB;
 
-create table if not exists SAP.tag(
-	id_tag int not null auto_increment,
-    nome_tag varchar(11) not null,
-    primary key (id_tag)
-)engine = InnoDB;
-
-create table if not exists SAP.tag_usuario(
-	id_tag_fk int not null,
-    id_usuario_fk int not null,
-    primary key (id_tag_fk,id_usuario_fk),
-    foreign key (id_usuario_fk) references usuario(id_usuario),
-    foreign key (id_tag_fk) references tag(id_tag)
-)engine = InnoDB;
-
-create table if not exists SAP.tag_chara(
-	id_tag_fk int not null,
-    id_chara_fk int not null,
-    primary key (id_tag_fk,id_chara_fk),
-    foreign key (id_chara_fk) references chara(id_chara),
-    foreign key (id_tag_fk) references tag(id_tag)
-)engine = InnoDB;
 
 create table if not exists SAP.comentario(
 	id_comentario int not null auto_increment,
     id_usuario_fk int not null,
     id_chara_fk int not null,
     mensagem_comentario varchar(300) not null,
-	data_hora_comentario datetime default(now()),
+	data_hora_comentario datetime default now(),
     primary key (id_comentario),
     foreign key (id_usuario_fk) references usuario(id_usuario),
     foreign key (id_chara_fk) references chara(id_chara)
@@ -128,20 +86,10 @@ create table if not exists SAP.notificacao(
 	id_notificacao int not null auto_increment,
     id_usuario_fk int not null,
     mensagem_notificacao varchar(300) not null,
-	data_hora_notificacao datetime default(now()),
+	data_hora_notificacao datetime default now(                  ),
     primary key (id_notificacao),
     foreign key (id_usuario_fk) references usuario(id_usuario)
 ) engine = InnoDB;
-
-/*
-create table if not exists SAP.multi_categoria(
-	id_cat_1 int not null,
-    id_cat_2 int not null,
-    primary key (id_cat_1, id_cat_2),
-    foreign key (id_cat_1) references categoria(id),
-    foreign key (id_cat_2) references categoria(id)
-) engine = InnoDB;
-*/
 
 insert into SAP.cargo (nome_cargo) values 
     ("Admin"),
@@ -152,88 +100,4 @@ insert into SAP.usuario (nome_usuario, email_usuario, senha_usuario, ativado_usu
 	("admin", "admin@admin.admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", true, 1),
     ("notadmin", "notadmin@user.admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", false, 3),
     ("jonh", "jojo@user.admin", "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918", true, 2);
-
-insert into categoria_chara (id_chara_fk, id_categoria_fk) values 
-    (3,1),
-    (4,1),
-    (5,2);
     
-insert into tag (nome_tag) values
-	("vermelho"),
-    ("verde"),
-    ("azul"),
-    ("amarelo"),
-    ("roxo");
-    
-insert into tag_usuario (id_tag_fk,id_usuario_fk) values
-		(1,1),
-        (2,1),
-        (3,1),
-        (4,2),
-        (5,2),
-        (1,3),
-        (4,3);
-
-insert into categoria_chara (id_chara_fk, id_categoria_fk) values (8,1);
-delete from categoria_chara where id_chara_fk = 8 and id_categoria_fk = 1;
-
-insert into notificacao (id_usuario_fk, mensagem_notificacao) values 
-	(1,"Shikononokonokonoshitantan"),
-    (1,"EEEEEEEEE"),
-    (1,"ShikoASDASDASDnonokonokonoshitantan"),
-    (1,"oisdfslkdfslkjdf");
-    
-/*
-insert into SAP.chat (id_usuario_1, id_usuario_2) values (1,2);
-insert into SAP.chat_mensagem (id_usuario,id_chat, mensagem, data_hora) values (1,1,"Mingo Bile","2023-05-20 09:20:03");
-
-select * from chat;
-select * from chat_mensagem order by data_hora;
-
-*/
-select * from usuario;
-select * from usuario where nome_usuario like '%jon%' or nome_usuario like '%ivo%';
-select * from usuario where nome_usuario like '%jon%' or nome_usuario like '%ivo%';
-select * from comentario;
-select * from notificacao;
-select count(*)as qtd from notificacao where id_usuario_fk = 1;
-select * from comentario where id_chara_fk = 4 order by data_hora_comentario DESC;
-
-select c.*, u.nome_usuario from comentario c 
-	inner join usuario u on c.id_usuario_fk = u.id_usuario
-	where id_chara_fk = 4 order by data_hora_comentario DESC;
-
-select length(senha_usuario) from usuario;
-select * from cargo; 
-select * from chara; 
-select u.id_usuario, u.nome_usuario, u.email_usuario, u.senha_usuario, 
-	case when u.ativado_usuario = 0 then 'não' else 'sim' end as ativado,
-    c.nome_cargo, u.id_cargo_fk from usuario u inner join 
-	cargo c on u.id_cargo_fk = c.id_cargo order by u.id_usuario;
-    
-    
-    
-Select * from usuario where ativado_usuario = false;
-Select * from chara;
-select * from categoria;
-select * from categoria_chara;
-
-select * from chara c inner join categoria_chara cc on
-	c.id_chara = cc.id_chara_fk where id_usuario_fk = 1;
-select * from chara c left join categoria_chara cc on
-	c.id_chara = cc.id_chara_fk where id_usuario_fk = 1 and id_categoria_fk = 1;    
-
-select * from categoria where id_usuario_fk = 1;
-select u.id_usuario, u.nome_usuario, u.email_usuario, u.senha_usuario, 	u.ativado_usuario,c.nome_cargo as cargo from usuario u inner join cargo c on u.id_cargo_fk = c.id_cargo order by u.id_usuario;
-
-select c.id_chara, c.id_usuario_fk, u.nome_usuario, c.nome_chara, c.descricao_chara, img_link_chara
-	from chara c inner join usuario u on c.id_usuario_fk = u.id_usuario;
-    
-select count(*) as qtd from usuario where id_usuario = 0;
-
-select * from chat;
-select cm.*,u.nome_usuario from chat_mensagem cm inner join usuario u on cm.id_usuario_fk = u.id_usuario where id_chat_fk = 2 order by data_hora_cm;
-
-insert into chat (id_usuario1_fk,id_usuario2_fk) select 3,1
-	where not exists (select 1 from chat where (id_usuario1_fk = 1 and id_usuario2_fk = 3) or (id_usuario1_fk = 3 and id_usuario2_fk = 1));
-update chara set id_usuario_fk = 2 where id_chara = 10; 
